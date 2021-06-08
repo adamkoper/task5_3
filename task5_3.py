9b4f12709db2045c7a8a8b19d7009bfdd06d8280
Adam Koprowski <adamkoprowski4@gmail.com>
2021-06-08 22:39:18 +0200

    add. buged sorted

--- task5_3.py
+++ task5_3.py
@@ -6,17 +6,29 @@ fake = Faker()
 
 
 class cards:
-   def __init__(self, name, company_name, mail):
+   def __init__(self, name, last_name, company_name, mail):
        self.name = name
+       self.last_name = last_name
        self.company_name = company_name
        self.mail = mail
    def __str__(self):
-       return f'{self.name} {self.company_name} {self.mail}'
+       return f'{self.name} {self.last_name} {self.company_name} {self.mail}'
+
+random_person = cards(name=fake.first_name(), last_name=fake.last_name(), company_name=fake.company(), mail=fake.email())
+print(random_person)
+name = [fake.first_name() for i in range(10)]
+company_name = [fake.company() for i in range(10)]
+mail = [fake.email() for x in range(10)]
+last_name = [fake.last_name() for x in range(10)]
+
+cards_list = cards(name=name, last_name=last_name, company_name=company_name, mail=mail)
+
+by_name = sorted(cards_list, key=lambda card: cards.name)
+by_last_name = sorted(cards_list, key=lambda card: cards.last_name)
+by_email = sorted(cards_list, key=lambda card: cards.mail)
 
 
-bubu = cards(name=fake.name(), company_name=fake.company(), mail=fake.email())
-print(bubu)
-
+print(cards_list)
 
 
 
