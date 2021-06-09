dd5bcf9c0da494d21abc13b4a0b216513c13b96a
Adam Koprowski <adamkoprowski4@gmail.com>
2021-06-09 17:11:04 +0200

    add function contact and property

--- task5_3.py
+++ task5_3.py
@@ -3,32 +3,39 @@ fake = Faker()
 
 
 class cards:
-    def __init__(self, name, last_name, company_name, mail):
+    def __init__(self, name, last_name, company_name, mail, job):
         self.name = name
         self.last_name = last_name
         self.company_name = company_name
         self.mail = mail
+        self.job = job
 
     def __str__(self):
-        return f'{self.name} {self.last_name} {self.company_name} {self.mail}'
+        return f'{self.name} {self.last_name} {self.job} {self.company_name} {self.mail}'
 
     def __repr__(self):
-        return f"Cards(name={self.name} last_name={self.last_name}, company_name={self.company_name}, mail={self.mail})"
+        return f"Cards(name={self.name} last_name={self.last_name}, job={self.job}, company_name={self.company_name}, mail={self.mail})"
+
+    def contact(self):
+        return f'Kontaktuję się z {self.name} {self.last_name} {self.job} {self.mail}'
+
+    @property
+    def name_lenght(self, name, last_name):
+        return len(name)+1+len(last_name)
 
 
-card1 = cards(name=fake.unique.first_name(), last_name=fake.unique.last_name(),
+card1 = cards(name=fake.unique.first_name(), last_name=fake.unique.last_name(), job=fake.job(),
               company_name=fake.unique.company(), mail=fake.unique.email())
-card2 = cards(name=fake.unique.first_name(), last_name=fake.unique.last_name(),
+card2 = cards(name=fake.unique.first_name(), last_name=fake.unique.last_name(), job=fake.job(),
               company_name=fake.unique.company(), mail=fake.unique.email())
-card3 = cards(name=fake.unique.first_name(), last_name=fake.unique.last_name(),
+card3 = cards(name=fake.unique.first_name(), last_name=fake.unique.last_name(), job=fake.job(),
               company_name=fake.unique.company(), mail=fake.unique.email())
-card4 = cards(name=fake.unique.first_name(), last_name=fake.unique.last_name(),
+card4 = cards(name=fake.unique.first_name(), last_name=fake.unique.last_name(), job=fake.job(),
               company_name=fake.unique.company(), mail=fake.unique.email())
-card5 = cards(name=fake.unique.first_name(), last_name=fake.unique.last_name(),
+card5 = cards(name=fake.unique.first_name(), last_name=fake.unique.last_name(), job=fake.job(),
               company_name=fake.unique.company(), mail=fake.unique.email())
 
 cards_list = [card1, card2, card3, card4, card5]
-
 
 by_name = sorted(cards_list, key=lambda cards: cards.name)
 by_last_name = sorted(cards_list, key=lambda cards: cards.last_name)
@@ -38,6 +45,8 @@ print(card1)
 print(by_name)
 print(by_last_name)
 print(by_email)
+print(cards.contact(card1))
+
 
 
 
--- test.py
+++ test.py
@@ -1,27 +1,16 @@
 class Car:
+
    def __init__(self, make, model_name, top_speed, color):
        self.make = make
        self.model_name = model_name
        self.top_speed = top_speed
        self.color = color
-   def __str__(self):
-       return f'{self.color} {self.make} {self.model_name}'
 
-   def __repr__(self):
-       return f"Car(make={self.make} model={self.model_name}, top_speed={self.top_speed}, color={self.color})"
-mustang = Car(make="Ford", model_name="Mustang", color="Yellow", top_speed=250)
-print(mustang)
+       # Variables
+       self.current_speed = 0
 
-car_one = Car(make="Ford", model_name="Mustang", top_speed=250, color="Red")
-car_two = Car(make="Ford", model_name="Fiesta", top_speed=200, color="Blue")
-car_three = Car(make="Porsche", model_name="911", top_speed=320, color="Black")
-cars = [car_one, car_two, car_three]
-by_speed = sorted(cars, key=lambda car: car.top_speed)
-by_make = sorted(cars, key=lambda car: car.make)
-print(cars)
+   def accelerate(self, step=10):
+       self.current_speed += step
 
-from faker import Faker
-fake = Faker()
-
-for x in range(10):
-    print(fake.name())
+   def decelerate(self, step=10):
+       self.current_speed -= step
